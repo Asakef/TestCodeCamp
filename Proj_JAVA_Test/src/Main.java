fde4217eddafedb5058e18417585fdcbe6d44bc5
Asakef <fmsfunky@gmail.com>
2022-09-23 09:34:45 +0100

    JAVA Task 14

---
+++ Proj_JAVA_Test/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello Testify!");
+        System.out.println("I am a student in Testify!");
+        System.out.println("Switch to Software Testing Course");
+        System.out.println("I am a  learning JAVA");
+        System.out.println("I am Selenium Automation!");
+        System.out.println("I will soon graduate from Testify Academy!");
+        System.out.println("Thank You!");
+
+
+    }
+}
